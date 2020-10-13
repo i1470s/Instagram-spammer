5653eaf7d9029571b0061c950e03b6acfeac0df9
i1470s <55937162+i1470s@users.noreply.github.com>
2020-10-13 16:59:14 -0700

    V.1.3

--- spammer.py
+++ spammer.py
@@ -1,7 +1,29 @@
 import pyautogui, time
 
-time.sleep(10)
-f = open("./libs/text.txt", 'r')
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
+choice = input("""
+$Created by i1470s#0396$
+
+
+[1] How it works this explains everything!
+[2] Spammer
+
+Made for brain dead skids with love <3
+
+Enter a option: """)
+
+
+if choice == '1':
+    print("""Hey Skid! this is a very simple program! if you want to change what the spammer sends 
+go to libs/text.txt and change the 10000 lines of pp poo poo to what ever you want!""")
+    time.sleep(100000)
+
+elif choice == '2':
+    print("Welcome your a dick for using this btw") 
+    time.sleep(2)
+    print("Starting spam in 10 seconds") 
+    time.sleep(10)
+    print("Spamming thy skid :P")
+    f = open("./libs/text.txt", 'r')
+    for word in f:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
